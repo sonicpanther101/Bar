cd545a2536ca09f15db865ef97b4513a57fe2fc1
sonicpanther101 <sonicpanther101@gmail.com>
2026-02-17 20:40:35 +1300

    workable ui

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <SableUI/SableUI.h>
+#include <cstdlib>
+#include <cstdio>
 
 using namespace SableUI;
 using namespace SableUI::Style;
-class Counter : public BaseComponent
+class Bar : public BaseComponent
 {
 public:
-    void Layout() override
-    {
+    void Layout() override {
         Text(SableString::Format("count: %d", count.get()), fontSize(18), mb(4));
         Button("Increment", [this]() { count.set(count.get() + 1); }, mr(4));
         Button("Decrement", [this]() { count.set(count.get() - 1); });
@@ -16,16 +17,28 @@ private:
     State<int> count{ this, 0 };
 };
 
-int main()
-{
-    RegisterComponent<Counter>("Counter");
+int main() {
+    RegisterComponent<Bar>("Bar");
 
-    InitialisePrimaryWindow("Counter", 800, 600);
+	// Get active monitor resolution from Hyprland
+    FILE* pipe = popen("hyprctl monitors -j | jq -r '.[] | select(.focused == true) | \"\\(.width) \\(.height)\"'", "r");
+    int width = 1920, height = 1080;  // defaults
+    if (pipe) {
+        if (fscanf(pipe, "%d %d", &width, &height) != 2) {
+            // Failed to read, use defaults
+            width = 1920;
+            height = 1080;
+        }
+        pclose(pipe);
+    }
 
-    Panel("Counter");
+    Window* window = InitialisePrimaryWindow("SableUI", width, 40);
+
+    Panel("Bar");
     
-    while (WaitEvents())
-        Render();
+	while (SableUI::WaitEventsTimeout(1/10)) {
+		Render();
+}
 
     Shutdown();
 }
